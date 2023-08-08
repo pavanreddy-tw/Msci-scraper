5033a11bec19462898a697be2f1510bd7bedc8c0
pavanreddy-tw <137087176+pavanreddy-tw@users.noreply.github.com>
2023-08-08 11:04:31 -0400

    Update scraper.py

--- MSCI Scraper/scraper.py
+++ MSCI Scraper/scraper.py
@@ -1,158 +1,244 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
-from bs4 import BeautifulSoup
-import requests
-import sys
+import pickle
+import datetime
 import os
 
-columns = ["Company Name", "Company Abbr", "Industry", "Country_Region", "decarbonization target",
-           "decarbonization target that is considered in the calculation of Implied Temperature Rise?",
-           "Target Year", "Comprehensiveness", "Ambition", "MSCI IMPLIED TEMPERATURE RISE",
-           "MSCI ESG RATING", "Environment", "Social", "Customers", "Anticompetitive Practices", "Privacy & Data Security", "Human Rights & Community",
-           "Labor Rights & Supply Chain", "Discrimination & Workforce Diversity", "Governance", "Banned Controversial Weapons", "Gambling", "Tobacco", "Alcohol"]
-
+session = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 data = None
 
+if len(os.listdir("CSV/")) >= 1:
+    scraped_tickers = pd.read_csv("CSV/" + os.listdir("CSV/")[-1])
+    scraped_tickers = scraped_tickers["temp_ticker"].to_list()
+else:
+    scraped_tickers = []
 
 
 def is_element_exists_by_xpath(driver, xpath):
     try:
-        element = driver.find_element(By.XPATH,xpath)
+        element = driver.find_element(By.XPATH, xpath)
         return True
     except NoSuchElementException:
         return False
 
 
-
-driver=webdriver.Chrome()
+with open("tickers.pickle", 'rb') as f:
+    tickers = pickle.load(f)
+f.close()
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-article_driver=webdriver.Chrome(options=chrome_options)
+article_driver = webdriver.Chrome(options=chrome_options)
 
 url = 'https://www.msci.com/our-solutions/esg-investing/esg-ratings-climate-search-tool/issuer/microsoft-corporation/IID000000002143620'
-driver.get(url)
 
 
+while len(tickers) - len(scraped_tickers) > 0:
+    for company in tickers:
+        try:
+            if company in scraped_tickers:
+                continue
+
+            driver = webdriver.Chrome()
+            driver.get(url)
+
+            company_data = dict()
+
+            if is_element_exists_by_xpath(driver, xpath='//*[@id="_esgratingsprofile_keywords"]'):
+                input_field = driver.find_element(By.XPATH, '//*[@id="_esgratingsprofile_keywords"]')
+                input_field.send_keys(company)
+                time.sleep(5)
+            else:
+                continue
+
+            if is_element_exists_by_xpath(driver, '//*[@id="ui-id-1"]/li'):
+                element = driver.find_element(By.XPATH, '//*[@id="ui-id-1"]/li')
+                driver.execute_script("arguments[0].click();", element)
+                time.sleep(5)
+
+                company_data["temp_ticker"] = company
+
+                company_data["Company Name"] = driver.find_element(By.XPATH,
+                                                                   '//*[@id="_esgratingsprofile_esg-ratings-profile-container"]/div[1]/h1').text
+                company_data["Ticker"] = driver.find_element(By.XPATH,
+                                                             '//*[@id="_esgratingsprofile_esg-ratings-profile-container"]/div[1]/div[1]').text
+                company_data["Industry"] = driver.find_element(By.XPATH,
+                                                               '//*[@id="_esgratingsprofile_esg-ratings-profile-container"]/div[1]/div[2]/div[1]').text
+                company_data["Country_Region"] = driver.find_element(By.XPATH,
+                                                                     '//*[@id="_esgratingsprofile_esg-ratings-profile-container"]/div[1]/div[2]/div[2]').text
+            else:
+                continue
+
+            if is_element_exists_by_xpath(driver, '//*[@id="esg-commitment-toggle-link"]'):
+                element = driver.find_element(By.XPATH,
+                                              '//*[@id="esg-commitment-toggle-link"]')
+                driver.execute_script("arguments[0].click();", element)
+                time.sleep(2)
+
+                try:
+                    company_data["decarbonization target"] = driver.find_element(By.XPATH,
+                                                                                 '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[1]/p[2]').text
+                except:
+                    pass
+
+                try:
+                    company_data[
+                        "decarbonization target that is considered in the calculation of Implied Temperature Rise?"] = driver.find_element(
+                        By.XPATH,
+                        '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[2]/p[2]').text
+                except:
+                    pass
+
+                try:
+                    company_data["Target Year"] = driver.find_element(By.XPATH,
+                                                                      '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[3]/p[2]').text
+                except:
+                    pass
+
+                try:
+                    company_data["Comprehensiveness"] = driver.find_element(By.XPATH,
+                                                                            '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[4]/p').text
+                except:
+                    pass
+
+                try:
+                    company_data["Ambition"] = driver.find_element(By.XPATH,
+                                                                   '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[5]/p').text
+                except:
+                    pass
+
+            if is_element_exists_by_xpath(driver, '//*[@id="esg-climate-toggle-link"]'):
+                element = driver.find_element(By.XPATH,
+                                              '//*[@id="esg-climate-toggle-link"]')
+                driver.execute_script("arguments[0].click();", element)
+                time.sleep(2)
+
+                try:
+                    company_data["MSCI IMPLIED TEMPERATURE RISE"] = driver.find_element(By.XPATH,
+                                                                                        '//*[@id="_esgratingsprofile_esg-company-transparency"]/div[1]/div[3]/span').text
+                except:
+                    pass
+
+            if is_element_exists_by_xpath(driver, '//*[@id="esg-transparency-toggle-link"]'):
+                element = driver.find_element(By.XPATH,
+                                              '//*[@id="esg-transparency-toggle-link"]')
+                driver.execute_script("arguments[0].click();", element)
+                time.sleep(5)
+
+                if is_element_exists_by_xpath(driver,
+                                              '//*[@id="_esgratingsprofile_esg-ratings-profile-header"]/div/div[1]/div[2]/div'):
+                    company_data["MSCI ESG RATING"] = driver.find_element(By.XPATH,
+                                                                          '//*[@id="_esgratingsprofile_esg-ratings-profile-header"]/div/div[1]/div[2]/div').get_attribute(
+                        'class').split('-')[-1].upper()
+
+            if is_element_exists_by_xpath(driver,
+                                          '//*[@id="esg-controversies-toggle-link"]'):
+                element = driver.find_element(By.XPATH,
+                                              '//*[@id="esg-controversies-toggle-link"]')
+                driver.execute_script("arguments[0].click();", element)
+                time.sleep(5)
+
+                for i in range(1, 20):
+                    try:
+                        company_data[f'cont_col{i}'] = \
+                        driver.find_element(By.XPATH, f'//*[@id="controversies-table"]/div[{i}]').get_attribute(
+                            'class').split(
+                            '-')[-1].title()
+                    except:
+                        continue
+
+            if is_element_exists_by_xpath(driver, '//*[@id="esg-involvement-toggle-link"]'):
+                element = driver.find_element(By.XPATH,
+                                              '//*[@id="esg-involvement-toggle-link"]')
+                driver.execute_script("arguments[0].click();", element)
+                time.sleep(2)
+
+                try:
+                    company_data["Banned Controversial Weapons"] = driver.find_element(By.XPATH,
+                                                                                       '//*[@id="esg-involvement-toggle"]/div/div[1]/div[3]/div[1]/div[1]/div[3]').text.title()
+                except:
+                    pass
+
+                try:
+                    company_data["Gambling"] = \
+                        driver.find_element(By.XPATH,
+                                            '//*[@id="esg-involvement-toggle"]/div/div[1]/div[3]/div[2]/div[1]/div[3]').text.title()
+                except:
+                    pass
+                try:
+                    company_data["Tobacco"] = \
+                        driver.find_element(By.XPATH,
+                                            '//*[@id="esg-involvement-toggle"]/div/div[1]/div[3]/div[3]/div[1]/div[3]').text.title()
+                except:
+                    pass
+
+                try:
+                    company_data["Alcohol"] = \
+                        driver.find_element(By.XPATH,
+                                            '//*[@id="esg-involvement-toggle"]/div/div[1]/div[3]/div[4]/div[1]/div[3]').text.title()
+                except:
+                    pass
+
+            if is_element_exists_by_xpath(driver, '//*[@id="esg-sdg-alignment-toggle-link"]'):
+                element = driver.find_element(By.XPATH, '//*[@id="esg-sdg-alignment-toggle-link"]')
+                driver.execute_script("arguments[0].click();", element)
+                time.sleep(2)
+
+                goals_list = [
+                    "No Poverty",
+                    "No Hunger",
+                    "Good Health and Well-Being",
+                    "Quality Education",
+                    "Gender Equality",
+                    "Clean Water and Sanitation",
+                    "Affordable and Clean Energy",
+                    "Decent Work and Economic Growth",
+                    "Industry, Innovation and Infrastructure",
+                    "Reduced Inequalities",
+                    "Sustainable Cities and Communities",
+                    "Responsible Consumption and Production",
+                    "Climate Action",
+                    "Life under Water",
+                    "Life on Land",
+                    "Peace, Justice and Strong Institutions",
+                    "Partnerships for the Goals"
+                ]
+
+                for i in range(2, 19):
+                    if is_element_exists_by_xpath(driver,
+                                                  f'//*[@id="esg-sdg-alignment-toggle"]/div/div[1]/div/div[{i}]'):
+                        company_data[goals_list[i - 2]] = driver.find_element(By.XPATH,
+                                                                              f'//*[@id="esg-sdg-alignment-toggle"]/div/div[1]/div/div[{i}]').get_attribute(
+                            'class').title()
+
+            for i in company_data.keys():
+                company_data[i] = [company_data[i]]
+
+            driver.quit()
+
+            if data is None:
+                data = pd.DataFrame.from_dict(company_data)
+            else:
+                data = pd.concat([data, pd.DataFrame.from_dict(company_data)], ignore_index=True)
+
+            data.to_csv(f"CSV/{session}.csv")
 
 
-for company in ["AAPL", "MSFT", "NFLX", "META"]:
-    company_data = dict()
+        except Exception as e:
+            if len(os.listdir("CSV/")) >= 1:
+                scraped_tickers = pd.read_csv("CSV/" + os.listdir("CSV/")[-1])
+                scraped_tickers = scraped_tickers["temp_ticker"].to_list()
+            else:
+                scraped_tickers = []
 
-    if is_element_exists_by_xpath(driver, xpath='//*[@id="_esgratingsprofile_keywords"]'):
-        input_field = driver.find_element(By.XPATH, '//*[@id="_esgratingsprofile_keywords"]')
-        input_field.send_keys(company)
-        time.sleep(5)
-    else:
-        continue
-
-    if is_element_exists_by_xpath(driver, '//*[@id="ui-id-1"]/li'):
-        element = driver.find_element(By.XPATH, '//*[@id="ui-id-1"]/li')
-        driver.execute_script("arguments[0].click();", element)
-        time.sleep(5)
-
-        company_data[columns[0]] = driver.find_element(By.XPATH, '//*[@id="_esgratingsprofile_esg-ratings-profile-container"]/div[1]/h1').text
-        company_data[columns[1]] = driver.find_element(By.XPATH,
-                                                           '//*[@id="_esgratingsprofile_esg-ratings-profile-container"]/div[1]/div[1]').text
-        company_data[columns[2]] = driver.find_element(By.XPATH, '//*[@id="_esgratingsprofile_esg-ratings-profile-container"]/div[1]/div[2]/div[1]').text
-        company_data[columns[3]] = driver.find_element(By.XPATH, '//*[@id="_esgratingsprofile_esg-ratings-profile-container"]/div[1]/div[2]/div[2]').text
-    else:
-        continue
-
-    dropdown_xpaths = ['//*[@id="esg-commitment-toggle-link"]',
-                       '//*[@id="esg-climate-toggle-link"]',
-                       '//*[@id="esg-transparency-toggle-link"]',
-                       '//*[@id="esg-controversies-toggle-link"]',
-                       '//*[@id="esg-involvement-toggle-link"]',
-                       '//*[@id="esg-sdg-alignment-toggle-link"]']
-
-    if is_element_exists_by_xpath(driver, '//*[@id="esg-commitment-toggle-link"]'):
-        element = driver.find_element(By.XPATH, '//*[@id="esg-commitment-toggle-link"]')
-        driver.execute_script("arguments[0].click();", element)
-        time.sleep(2)
-
-        company_data[columns[4]] = driver.find_element(By.XPATH,
-                                                       '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[1]/p[2]').text
-        company_data[columns[5]] = driver.find_element(By.XPATH,
-                                                       '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[2]/p[2]').text
-        company_data[columns[6]] = driver.find_element(By.XPATH,
-                                                       '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[3]/p[2]').text
-        company_data[columns[7]] = driver.find_element(By.XPATH,
-                                                       '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[4]/p').text
-        company_data[columns[8]] = driver.find_element(By.XPATH,
-                                                      '//*[@id="esg-commitment-toggle"]/div/div[1]/div/div[2]/div[5]/p').text
-
-    if is_element_exists_by_xpath(driver, '//*[@id="esg-climate-toggle-link"]'):
-        element = driver.find_element(By.XPATH, '//*[@id="esg-climate-toggle-link"]')
-        driver.execute_script("arguments[0].click();", element)
-        time.sleep(2)
-
-        company_data[columns[9]] = driver.find_element(By.XPATH,
-                                                       '//*[@id="_esgratingsprofile_esg-company-transparency"]/div[1]/div[3]/span').text
-
-    if is_element_exists_by_xpath(driver, '//*[@id="esg-transparency-toggle-link"]'):
-        element = driver.find_element(By.XPATH, '//*[@id="esg-transparency-toggle-link"]')
-        driver.execute_script("arguments[0].click();", element)
-        time.sleep(5)
-
-        company_data[columns[10]] = driver.find_element(By.XPATH, '//*[@id="_esgratingsprofile_esg-ratings-profile-header"]/div/div[1]/div[2]/div').get_attribute('class').split('-')[-1].upper()
-
-    if is_element_exists_by_xpath(driver, '// *[ @ id = "esg-controversies-toggle-link"]'):
-        element = driver.find_element(By.XPATH, '// *[ @ id = "esg-controversies-toggle-link"]')
-        driver.execute_script("arguments[0].click();", element)
-        time.sleep(2)
-
-        company_data[columns[11]] = driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[1]').get_attribute('class').split('-')[-1].title()
-        company_data[columns[12]] = \
-        driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[2]').get_attribute('class').split('-')[
-            -1].title()
-        company_data[columns[13]] = \
-        driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[3]').get_attribute('class').split('-')[
-            -1].title()
-        company_data[columns[14]] = \
-        driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[4]').get_attribute('class').split('-')[
-            -1].title()
-        company_data[columns[15]] = \
-        driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[7]').get_attribute('class').split('-')[
-            -1].title()
-        company_data[columns[16]] = \
-            driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[10]').get_attribute('class').split('-')[
-                -1].title()
-        company_data[columns[17]] = \
-            driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[11]').get_attribute('class').split('-')[
-                -1].title()
-        company_data[columns[18]] = \
-            driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[14]').get_attribute('class').split('-')[
-                -1].title()
-        company_data[columns[18]] = \
-            driver.find_element(By.XPATH, '//*[@id="controversies-table"]/div[19]').get_attribute('class').split('-')[
-                -1].title()
-
-
-    if is_element_exists_by_xpath(driver, '//*[@id="esg-involvement-toggle-link"]'):
-        element = driver.find_element(By.XPATH, '//*[@id="esg-involvement-toggle-link"]')
-        driver.execute_script("arguments[0].click();", element)
-        time.sleep(2)
-
-        company_data[columns[19]] = driver.find_element(By.XPATH, '//*[@id="esg-involvement-toggle"]/div/div[1]/div[3]/div[1]/div[1]/div[3]').get_attribute('class').split('-')[-1].title()
-        company_data[columns[20]] = \
-        driver.find_element(By.XPATH, '//*[@id="esg-involvement-toggle"]/div/div[1]/div[3]/div[2]/div[1]/div[3]').get_attribute('class').split('-')[
-            -1].title()
-        company_data[columns[21]] = \
-        driver.find_element(By.XPATH, '//*[@id="esg-involvement-toggle"]/div/div[1]/div[3]/div[3]/div[1]/div[3]').get_attribute('class').split('-')[
-            -1].title()
-        company_data[columns[22]] = \
-        driver.find_element(By.XPATH, '//*[@id="esg-involvement-toggle"]/div/div[1]/div[3]/div[4]/div[1]/div[3]').get_attribute('class').split('-')[
-            -1].title()
-
-    if data is None:
-        data = pd.DataFrame(company_data, index=[0])
-    else:
-        data = data.append(pd.DataFrame(company_data, index=[0]), ignore_index=True)
-
-
-print(data)
+            print(e)
+            driver.quit()
+            continue
